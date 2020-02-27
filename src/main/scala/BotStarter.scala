78cac12e35c1e9ef35203a086f75885e69de9947
Evgeny Im <evgenyim6456@gmail.com>
2020-02-27 17:05:17 +0300

    add base bot

---
+++ src/main/scala/BotStarter.scala
@@ -0,0 +1,47 @@
+package bot
+
+import cats.instances.future._
+import cats.syntax.functor._
+import com.bot4s.telegram.api.RequestHandler
+import com.bot4s.telegram.api.declarative.Commands
+import com.bot4s.telegram.clients.{FutureSttpClient, ScalajHttpClient}
+import com.bot4s.telegram.future.{Polling, TelegramBot}
+import com.bot4s.telegram.models.User
+import com.softwaremill.sttp.SttpBackendOptions
+import com.softwaremill.sttp.okhttp.{OkHttpBackend, OkHttpFutureBackend}
+import slogging.{LogLevel, LoggerConfig, PrintLoggerFactory}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+class BotStarter(override val client: RequestHandler[Future]) extends TelegramBot
+  with Polling
+  with Commands[Future] {
+
+  var users: List[User] = List()
+  onCommand("/start") { implicit msg =>
+    val user = msg.from match {
+      case Some(x) => x
+    }
+    users = user :: users
+    reply(s"Hi!").void
+  }
+
+  onCommand("/users") { implicit msg =>
+    reply(users.toString()).void
+  }
+
+}
+
+object BotStarter {
+  def main(args: Array[String]): Unit = {
+    implicit val ec: ExecutionContext = ExecutionContext.global
+    implicit val backend = OkHttpFutureBackend(
+      SttpBackendOptions.Default.socksProxy("ps8yglk.ddns.net", 11999)
+    )
+
+    val token = ""
+    val bot = new BotStarter(new FutureSttpClient(token))
+    Await.result(bot.run(), Duration.Inf)
+  }
+}
